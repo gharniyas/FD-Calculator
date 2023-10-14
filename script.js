68e70a9d32e4e1d0eff32126a3b22dbc3ffe3496
gharniyas <gharniyasramesh@gmail.com>
2023-10-14 11:14:32 +0530

    fd

---
+++ script.js
@@ -0,0 +1,13 @@
+function Fdrate(){
+    let months=Number(document.getElementById("input").value)
+    if(months<3)
+    rate=5.8
+    else if(months>=3 && months<=6)
+    rate=6.5
+    else if(months>=7 && months<=9)
+    rate=6.8
+    else
+    rate=7
+    let result=document.querySelector('#result')
+    result.innerHTML="your interest rate is "+rate +"%"
+}
